eaf065f8b47e2e305b46cfbd49c93db6babaea62
codingNoob12 <ilhyeon0326@gmail.com>
2024-11-28 14:45:25 +0000

    [2024-11-28] leetcode 234: 팰린드롬 연결 리스트 * 러너를 이용한 풀이   * 테스트코드   * 소스코드 * 스택을 이용한 풀이   * 테스트코드   * 소스코드

---
+++ src/test/java/solution/leetcode/linkedlist/PalindromeLinkedListTest.java
@@ -0,0 +1,46 @@
+package solution.leetcode.linkedlist;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import solution.leetcode.types.ListNode;
+
+public class PalindromeLinkedListTest {
+
+    PalindromeLinkedList sut = new PalindromeLinkedList();
+
+    @ParameterizedTest
+    @DisplayName("연결리스트의 head가 주어지면, 해당 연결리스트가 팰린드롬을 만족하는지를 결과로 리턴한다.")
+    @MethodSource("provider")
+    public void givenLinkedList_whenCheckWhetherItIsPalindrome_thenReturnTheResult(ListNode head, boolean expected) {
+        boolean actual = sut.isPalindrome(head);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static ListNode createList(int[] values) {
+        ListNode root = new ListNode();
+        ListNode node = root;
+        for (int value : values) {
+            node.next = new ListNode(value);
+            node = node.next;
+        }
+        return root.next;
+    }
+
+    private static Stream<Arguments> provider() {
+        return Stream.of(
+            Arguments.of(createList(new int[] {1, 2, 2, 1}), true),
+            Arguments.of(createList(new int[] {1, 2}), false),
+            Arguments.of(createList(new int[] {}), true),
+            Arguments.of(createList(new int[] {1}), true),
+            Arguments.of(createList(new int[] {1, 2, 3, 2, 1}), true)
+        );
+    }
+}
