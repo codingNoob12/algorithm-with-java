a6275f200eb2261af766d1278faf3f7a23bee6f7
codingNoob12 <ilhyeon0326@gmail.com>
2024-11-28 14:37:02 +0000

    [2024-11-28] ListNode 클래스 equals 오버라이드 테스트시 AssertJ의 isEqualTo메서드가 equals메서드로 검증함. 그래서, 두 리스트가 동일한지 검증하는 로직에서 equals 메서드를 다음과 같이 오버라이드 해두면 검증이 쉬워질 것 같아 구현함.

--- src/main/java/solution/leetcode/types/ListNode.java
+++ src/main/java/solution/leetcode/types/ListNode.java
@@ -16,4 +16,26 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj instanceof ListNode) {
+            ListNode node = (ListNode) obj;
+            if (node.next == null && this.next == null) {
+                return true;
+            }
+
+            if (node.next == null || this.next == null) {
+                return false;
+            }
+            
+            return this.next.equals(node.next);
+        }
+
+        return false;
+    }
 }
